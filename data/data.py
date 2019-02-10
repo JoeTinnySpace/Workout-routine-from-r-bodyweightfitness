0fe16aa1e076a027e887c7831e8dad9e81a707e3
joe <josephdennisjoo71@gmail.com>
2019-02-10 22:56:58 +0530

    Initial commit

---
+++ data/data.py
@@ -0,0 +1,96 @@
+
+class warmups():
+    excercise1 = {'reps': '5-10', 'name': 'Yuri\'s Shoulder Band Warmup', 'link':'https://www.youtube.com/watch?v=Vwn5hSf3WEg', 'comments': 'Less good: Stick dislocates, can also be done with a tee-shirt'}
+    excercise2 = {'reps': '5-10', 'name': 'Squat Sky Reaches', 'link': 'https://youtu.be/lbozu0DPcYI?t=42s', 'comments': 'You can do these assisted'}
+    excercise3 = {'reps': '10+', 'name': 'GMB Wrist Prep', 'link': 'https://www.youtube.com/watch?v=mSZWSQSSEjE', 'comments': 'Do as many reps as you want'}
+    excercise4 = {'reps': '30s', 'name': 'Deadbugs', 'link': 'http://www.nick-e.com/deadbug/', 'comments': 'None'}
+    excercise5 = {'reps': '10', 'name': 'Arch Hangs', 'link': 'https://youtu.be/C995b3KLXS4?t=7s', 'comments': 'Add these after you reach Negative Pullups.'}
+    excercise6 = {'reps': '30s', 'name': 'Support Hold', 'link': 'https://www.reddit.com/r/bodyweightfitness/wiki/exercises/support', 'comments': 'Add these after you reach Negative Dips.'}
+    excercise7 = {'reps': '10', 'name': 'Easier Squat progression', 'link': 'https://www.reddit.com/r/bodyweightfitness/wiki/exercises/squat', 'comments': 'Add these after you reach Bulgarian Split Squats.'}
+
+
+class workouts():
+    pair1 = [ {'sets': '3', 'reps': '5-8', 'name': 'Pull-up progression', 'progressions': {
+                                                                                            0:{'prog_name':'Scapular Pulls','link':'https://youtu.be/FgYoc4O-cio?t=1m21s','desc':'Elbows should stay straight, Your back will arch more as you get stronger,These should be performed with a pause at the "top". Squeeze your shoulders, hold it for 3-5s then release into a dead hang under control, If you can\'t pull out of the dead hang at all, consider using bands or your feet to assist you until the gain the necessary strength.'},
+                                                                                            1:{'prog_name':'Arch Hangs','link':'https://youtu.be/C995b3KLXS4?t=7s','desc':'Elbows should stay straight, Hold it for time/reps'},
+                                                                                            2:{'prog_name':'Pull-up Negatives','link':'https://vimeo.com/76666801','desc':'Jump to the top of the pull-up position, then slowly (as slowly as you can), lower yourself until your arms are straight. Build up to 10sec negatives!'},
+                                                                                            3:{'prog_name':'Pull-ups','link':'https://www.youtube.com/watch?v=eGo4IYlbE5g','desc':''}
+                                                                                            }
+              },
+              {'sets': '3', 'reps': '5-8', 'name': 'Squat Progression', 'progressions': {
+                                                                                            0:{'prog_name':'Assisted Squat','link':'https://www.youtube.com/watch?v=OuR_Fp7AB0c','desc':'Grab something in front of you and use your hands to assist in the squat. Reduce assistance over time.'},
+                                                                                            1:{'prog_name':'Squat','link':'https://www.youtube.com/watch?v=zJBLDJMJiDE','desc':', Stand up straight at the top, Go as low as you can, preferably until the hips are below the knees, Dig your big toe and heel into the ground, Keep your knee in-line with your toes, Don\'t let the knees come inward on either the descend or the ascend; think about pushing the knees out'},
+                                                                                            2:{'prog_name':'Split Squat','link':'https://www.getstrong.fit/images/GobletSplitSquat.jpg','desc':', Pretend the dumbbell doesn\'t exist., Remain in the split stance throughout the set. Unlike a lunge, you don\'t return to a "feet together" stance.'},
+                                                                                            3:{'prog_name':'Bulgarian Split Squat','link':'https://www.youtube.com/watch?v=kkdmHTASZg8&feature=youtu.be&t=1m15s','desc':', Pretend the barbell doesn\'t exist., You can progress this further by elevating both your legs'},
+                                                                                            4:{'prog_name':'Beginner Shrimp Squat','link':'https://www.youtube.com/watch?v=TKt0-c83GSc&feature=youtu.be&t=3m9s','desc':'Make sure your knee and toes leave the floor at the same time '},
+                                                                                            5:{'prog_name':'Intermediate Shrimp Squat','link':'','desc':'In this variation, the knee touches the ground but the toes do not.'},
+                                                                                            6:{'prog_name':'Advanced Shrimp Squat','link':'','desc':'In this variation, you hold your heel as you descend. only the knee touches the floor.'}
+                                                                                            }
+              } 
+            ]
+    pair2 = [ {'sets': '3', 'reps': '5-8', 'name': 'Dip progression', 'progressions': {
+                                                                                            0:{'prog_name':'Parallel Bar Support Hold','link':'http://antranik.org/wp-content/uploads/2014/01/antranik-holding-support-hold-on-parallel-bars.jpg','desc':'Work up to 3 sets of 1 minute holds for this progression'},
+                                                                                            1:{'prog_name':'Negative Dips','link':'https://www.youtube.com/watch?v=T3Scqw1BbCc','desc':'This exercise is only the descent. To get to the starting position you can jump or use a box or anything., Lower yourself with control. Work up to 10 sec descents'},
+                                                                                            2:{'prog_name':'Parallel Bar Dips','link':'https://www.youtube.com/watch?v=2z8JmcrW-As','desc':'Straight or slightly hollow body. No bending at the hips!, Lock out the arms and depress the shoulderblades (what does that even mean?) at the top, Go as far down as you can, Elbows in'},
+                                                                                            3:{'prog_name':'Weighted Dips','link':'','desc':''}
+                                                                                            }
+              },
+              {'sets': '3', 'reps': '5-8', 'name': 'Hinge Progression', 'progressions': {
+                                                                                            0:{'prog_name':'Romanian Deadlift','link':'https://gfycat.com/BlueUltimateBaiji','desc':'Stand up straight. Brace and squeeze your butt to flatten your low back (neutral spine to maybe slightly arched)., Send your butt back and lean your torso forward to avoid falling over, You can place the hands on your chest and abdomen for feedback or let them hang by your sides.'},
+                                                                                            1:{'prog_name':'Single Legged Deadlift','link':'https://www.youtube.com/watch?v=iDV8r5u6En0','desc':'Please ignore the barbell., If you lack the balance to perform these with a leg in the air, you can use an arm to brace yourself against a surface or you can straddle your legs to help with balance. '},
+                                                                                            2:{'prog_name':'Banded Nordic Curl Negatives','link':'https://www.youtube.com/watch?v=HUXS3S2xSX4','desc':'Anchor your feet under something sturdy. You want to be anchored about a fist\'s distance away from your ankle., Squeeze your butt to keep your hips, Slowly lower your torso, avoiding piking at the hips, Do a little push-up when you reach the bottom to get back to the top and "skip" the concentric'},
+                                                                                            3:{'prog_name':'Banded Nordic Curl','link':'https://www.youtube.com/watch?v=HUXS3S2xSX4','desc':'Anchor your feet under something sturdy. You want to be anchored about a fist\'s distance away from your ankle., Squeeze your butt to keep your hips, Slowly lower your torso, avoiding piking at the hips'},
+                                                                                            4:{'prog_name':'Nordic Curls','link':'https://www.youtube.com/watch?v=d8AAPcYxPo8','desc':'Anchor your feet under something sturdy. You want to be anchored about a fist\'s distance away from your ankle., Squeeze your butt to keep your hips, Slowly lower your torso, avoiding piking at the hips'},
+                                                                                            }
+              } 
+            ]
+    pair3 = [ {'sets': '3', 'reps': '5-8', 'name': 'Row Progression', 'progressions': {
+                                                                                            0:{'prog_name':'Vertical Rows','link':'https://youtu.be/rloXYB8M3vU','desc':'Perform a row, but with the body position vertical.'},
+                                                                                            1:{'prog_name':'Incline Rows','link':'https://www.youtube.com/watch?v=LR2EnFWpVao','desc':'Perform a row, but with the body position somewhere between vertical and horizontal. Go closer to horizontal to make it harder.'},
+                                                                                            2:{'prog_name':'Horizontal Rows','link':'https://www.youtube.com/watch?v=dvkIaarnf0g','desc':''},
+                                                                                            3:{'prog_name':'Wide Rows','link':'http://www.youtube.com/watch?v=1yMRvsuk9Xg','desc':'It\'s okay to flare your elbows on this one., On a bar: take a wide grip (1.5 times shoulder width) and perform rows like this., On rings: while performing a row, bring out the elbows to the side up to a 90 degree angle.'},
+                                                                                            4:{'prog_name':'Weighted Inverted Rows','link':'','desc':'These can be done with a weight plate on your chest/belly (uncomfortable and unreliable), or with a weight vest (minimal setup, but limited weight), or with a dip belt hanging from your waist or chest (This method can be loaded the most, however you need to elevate your entire body so that the plates don\'t touch the floor at the bottom). Alternatively, you can do Barbell or Dumbbell rows.'}
+                                                                                            }
+              },
+              {'sets': '3', 'reps': '5-8', 'name': 'Push-up progression', 'progressions': {
+                                                                                            0:{'prog_name':'Vertical Pushup','link':'','desc':'Standing next to a wall, put your hands on the wall and perform a pushup.'},
+                                                                                            1:{'prog_name':'Incline Pushup','link':'https://youtu.be/4dF1DOWzf20?t=3m56s','desc':'Finding some raised object, put your hands on said object and perform a pushup. Lower the height to increase the difficulty.'},
+                                                                                            2:{'prog_name':'Full Pushup','link':'https://www.youtube.com/watch?v=IODxDxX7oi4','desc':'Body in a straight line from head to toe: don\'t let the hips sag!, Lock out arms and protract the shoulderblades (what does that even mean?) at the top, Go down until chest nearly touches the ground, Keep the elbows in, don\'t let them flare out, Don\'t shrug up your shoulders to your ears, focus on depressing the shoulderblades'},
+                                                                                            3:{'prog_name':'Diamond Pushup','link':'https://www.youtube.com/watch?v=_4EGPVJuqfA','desc':'Put your hands close together so the thumbs and index fingers touch, then perform a pushup., If this is too difficult or feels uncomfortable, put your hands just a bit closer than in a normal pushup. Work on moving the hands closer together over time until you reach diamond pushups.'},
+                                                                                            4:{'prog_name':'Pseudo Planche Pushups','link':'http://www.youtube.com/watch?v=Cdmg0CfMZeo','desc':'Putting your hands sideways, lean forward with a straight body until your shoulders are in front of your hands. Perform a pushup while maintaining forward lean. Protract the shoulderblades at the top., Increase forward lean to make more difficult. Decrease forward lean to make easier. You won\'t run out of resistance anytime soon if you keep increasing your forward lean.'},
+                                                                                            }
+              } 
+            ]
+    pair4 = [ {'sets': '3', 'reps': '8-12', 'name': 'Anti-Extension progression', 'progressions': {
+                                                                                            0:{'prog_name':'Plank','link':'https://www.youtube.com/watch?v=44ScXWFaVBs&feature=youtu.be&t=10s','desc':'Work up to a 30s hold then move on to your ring rollouts, You\'re only doing the planks, don\'t do every drill in the video.'},
+                                                                                            1:{'prog_name':'Ring Ab Rollouts','link':'https://www.youtube.com/watch?v=LBUfnmugKLw','desc':'Elbows should stay straight, Remain in a hollow body position (ribs down, butt tucked), Keep your hands as close as your overhead mobility will allow., Higher ring position will make this easier. Elevating the feet will make it harder.'}
+                                                                                            }
+              },
+              {'sets': '3', 'reps': '8-12', 'name': 'Anti-Rotation progression', 'progressions': {
+                                                                                            0:{'prog_name':'Banded Pallof Press','link':'https://www.youtube.com/watch?v=AH_QZLm_0-s','desc':'These are performed under control with a short pause when your arms are fully extended'},
+                                                                                            }
+              },
+              {'sets': '3', 'reps': '8-12', 'name': 'Extension progression', 'progressions': {
+                                                                                            0:{'prog_name':'Reverse Hyperextension','link':'https://www.youtube.com/watch?v=ZeRsNzFcQLQ&','desc':'Keep your butt tucked'}
+                                                                                            }
+              }
+            ]    
+    
+    workout = {'instructions':' There are nine exercises. These exercises are to be done in pairs and triplets to save time. Pairing two exercises means doing a set of the first exercise, resting 90 seconds, then doing a set of the second exercise, resting 90 seconds, and repeating until you\'ve done 3 sets of that pair. For the triplet, you do a set of the first exercise, rest 60 seconds, do a set of the second exercise, rest 60 seconds, do a set of the third exercise, rest 60 seconds, and repeating until you\'ve done 3 sets of that triplet.', 
+                'rest_time':'If 90 seconds is not enough, you can rest up to 3 minutes if you like.',
+                'tempo':'Ideally, all these exercises are to be done in a "10X0" (1,0,X,O) tempo. That means 1 second down, no pause at the bottom, explode up and no pause at the top. When "exploding up", if the actual movement is slow, that\'s okay, it\'s the intent that matters.'}
+
+goals = ['Strength', 'Muscle Gain ', 'Fat Loss ']
+redditLink = 'https://www.reddit.com/r/bodyweightfitness/wiki/kb/recommended_routine'
+
+# values for lists are 0,1,2 (2 applies only for triple1)
+# sets = workouts.pair1[0].get('sets',{})  
+# reps = workouts.pair1[0].get('reps',{})
+# workout_name = workouts.pair1[0].get('name',{})
+
+# 
+# progressin_name = workouts.pair1[0].get('progressions',{}).get(0,{}).get('prog_name')
+# progressin_desc = workouts.pair1[0].get('progressions',{}).get(0,{}).get('desc')
+# progressin_link = workouts.pair1[0].get('progressions',{}).get(0,{}).get('link')
+
+# print(f'{progressin_name}\n{progressin_link}\n\n{progressin_desc}')
---
+++ workoutCLI.py
@@ -0,0 +1,186 @@
+from data import data
+
+mainChoice = 0
+
+def warmup():
+    ex1name = data.warmups.excercise1.get('name')
+    ex2name = data.warmups.excercise2.get('name')
+    ex3name = data.warmups.excercise3.get('name')
+    ex4name = data.warmups.excercise4.get('name')
+    ex5name = data.warmups.excercise5.get('name')
+    ex6name = data.warmups.excercise6.get('name')
+    ex7name = data.warmups.excercise7.get('name')
+    print(f'\n0. Back \n1. {ex1name} \n2. {ex2name} \n3. {ex3name} \n4. {ex4name} \n5. {ex5name} \n6. {ex6name} \n7. {ex7name} \n')
+    try:
+        warmChoice = (int)(input(''))
+    except:
+        print('Only digits(0-7) allowed!')
+    while warmChoice != 0:
+        if warmChoice == 1:
+            for key, val in data.warmups.excercise1.items():
+                print(f'{key}  --> {val} \n')
+            print('Press \'Enter\' to go back to warm up menu\n')
+            input('')
+            pass
+        elif warmChoice == 2:
+            for key, val in data.warmups.excercise2.items():
+                print(f'{key}  --> {val} \n')
+            print('Press \'Enter\' to go back to warm up menu\n')
+            input('')
+            pass
+        elif warmChoice == 3:
+            for key, val in data.warmups.excercise3.items():
+                print(f'{key}  --> {val} \n')
+            print('Press \'Enter\' to go back to warm up menu\n')
+            input('')
+            pass
+        elif warmChoice == 4:
+            for key, val in data.warmups.excercise4.items():
+                print(f'{key}  --> {val} \n')
+            print('Press \'Enter\' to go back to warm up menu\n')
+            input('')
+            pass
+        elif warmChoice == 5:
+            for key, val in data.warmups.excercise5.items():
+                print(f'{key}  --> {val} \n')
+            print('Press \'Enter\' to go back to warm up menu\n')
+            input('')
+            pass
+        elif warmChoice == 6:
+            for key, val in data.warmups.excercise6.items():
+                print(f'{key}  --> {val} \n')
+            print('Press \'Enter\' to go back to warm up menu\n')
+            input('')
+            pass
+        elif warmChoice == 7:
+            for key, val in data.warmups.excercise7.items():
+                print(f'{key}  --> {val} \n')
+            print('Press \'Enter\' to go back to warm up menu\n')
+            input('')
+            pass
+        else:
+            print('Okay. I get it... Your forarms are pumped. But you must press the right key bud!\n')
+        
+        print(f'\n0. Back \n1. {ex1name} \n2. {ex2name} \n3. {ex3name} \n4. {ex4name} \n5. {ex5name} \n6. {ex6name} \n7. {ex7name} \n')
+        try:
+            warmChoice = (int)(input(''))
+        except:
+            print('Okay. I get it... Your forarms are pumped. But you must press the right key bud!\n')
+
+
+def workout():
+    progChoice = -1
+    print('\n0. Back \n1. First pair \n2. Second Pair \n3. Third pair \n4. Triplet \n5. Instructions \n')
+    try:
+        workoutChoice = (int)(input(''))
+    except:
+        print('Only digits(0-7) allowed!')
+    
+    while workoutChoice != 0:
+        
+        if workoutChoice == 1:
+            print(f'\n0. Back \n1. ' + data.workouts.pair1[0].get('name') + '\n2. '+ data.workouts.pair1[1].get('name') +' \n')
+            progChoice = (int)(input(''))
+            while progChoice != 0:
+                if progChoice == 1:
+                    progressionPair1(0)
+                    pass
+                elif progChoice == 2:
+                    progressionPair1(1)
+                    pass
+                print(f'\n0. Back \n1. ' + data.workouts.pair1[0].get('name') + '\n2. '+ data.workouts.pair1[1].get('name') +' \n')
+                progChoice = (int)(input(''))
+        elif workoutChoice == 2:
+            print(f'\n0. Back \n1. ' + data.workouts.pair2[0].get('name') + '\n2. '+ data.workouts.pair2[1].get('name') +' \n')
+            progChoice = (int)(input(''))
+            while progChoice != 0:
+                if progChoice == 1:
+                    progressionPair2(0)
+                    pass
+                elif progChoice == 2:
+                    progressionPair2(1)
+                    pass
+                print(f'\n0. Back \n1. ' + data.workouts.pair2[0].get('name') + '\n2. '+ data.workouts.pair2[1].get('name') +' \n')
+                progChoice = (int)(input(''))
+        elif workoutChoice == 3:
+            print(f'\n0. Back \n1. ' + data.workouts.pair3[0].get('name') + '\n2. '+ data.workouts.pair3[1].get('name') +' \n')
+            progChoice = (int)(input(''))
+            while progChoice != 0:
+                if progChoice == 1:
+                    progressionPair3(0)
+                    pass
+                elif progChoice == 2:
+                    progressionPair3(1)
+                    pass
+                print(f'\n0. Back \n1. ' + data.workouts.pair3[0].get('name') + '\n2. '+ data.workouts.pair3[1].get('name') +' \n')
+                progChoice = (int)(input(''))
+        elif workoutChoice == 4:
+            print(f'\n0. Back \n1. ' + data.workouts.pair4[0].get('name') + '\n2. '+ data.workouts.pair4[1].get('name') +' \n3. '+ data.workouts.pair4[2].get('name') +' \n')
+            progChoice = (int)(input(''))
+            while progChoice != 0:
+                if progChoice == 1:
+                    progressionPair4(0)
+                    pass
+                elif progChoice == 2:
+                    progressionPair4(1)
+                    pass
+                elif progChoice == 3:
+                    progressionPair4(2)
+                    pass
+                print(f'\n0. Back \n1. ' + data.workouts.pair1[0].get('name') + '\n2. '+ data.workouts.pair1[1].get('name') +' \n3. '+ data.workouts.pair4[2].get('name') +' \n')
+                progChoice = (int)(input(''))
+        elif workoutChoice == 5:
+            print(data.workouts.workout.get('instructions') + '\n\n' +
+                data.workouts.workout.get('rest_time') + '\n\n' +
+                    data.workouts.workout.get('tempo') + '\n' )
+            pass
+        else:
+            pass
+        
+        print('\n0. Back \n1. First pair \n2. Second Pair \n3. Third pair \n4. Triplet \n5. Instructions \n')
+        try:
+            workoutChoice = (int)(input(''))
+        except:
+            pass
+
+def progressionPair1(x):
+    for key, _ in data.workouts.pair1[x].get('progressions',{}).items():
+        for key1, val1 in data.workouts.pair1[x].get('progressions',{}).get(key,{}).items():
+            print(key1 + ' >> ' + val1)
+        print('\n')
+
+def progressionPair2(x):
+    for key, _ in data.workouts.pair2[x].get('progressions',{}).items():
+        for key1, val1 in data.workouts.pair2[x].get('progressions',{}).get(key,{}).items():
+            print(key1 + ' >> ' + val1)
+        print('\n')
+
+def progressionPair3(x):
+    for key, _ in data.workouts.pair3[x].get('progressions',{}).items():
+        for key1, val1 in data.workouts.pair3[x].get('progressions',{}).get(key,{}).items():
+            print(key1 + ' >> ' + val1)
+        print('\n')
+
+def progressionPair4(x):
+    for key, _ in data.workouts.pair4[x].get('progressions',{}).items():
+        for key1, val1 in data.workouts.pair4[x].get('progressions',{}).get(key,{}).items():
+            print(key1 + ' >> ' + val1)
+        print('\n')
+
+
+
+while mainChoice != 3:
+    print('\nSelect from the following list...\n')
+    print('1. Warmup excercises \n2. Workouts \n3. Exit\n')
+    try:
+        mainChoice = (int)(input(''))
+    except:
+        print('Only digits(1-3) allowed!')
+    if mainChoice == 1:
+        warmup()
+    elif mainChoice == 2:
+        workout()
+    elif mainChoice == 3:
+        pass
+    else:
+        print('Wrong choice...')
